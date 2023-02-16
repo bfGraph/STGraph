200cf01dab0632418d5e9e345dd32ec4f1a3ddb8
nithinmanoj10 <nithinp.manoj@gmail.com>
2023-02-16 19:05:39 +0530

    ➕ Add device_info.py
    + Replicated the device_info.py module using the CUDA Python API
    + Added necessary macros for getting device attributes in device_info.py
    + Added pretty printing for getting device log

--- python/seastar/code_gen/cuda_python/cuda_driver.py
+++ python/seastar/code_gen/cuda_python/cuda_driver.py
@@ -1,9 +1,12 @@
-from cuda import cuda, nvrtc
+from cuda import cuda
 
 cuInit = cuda.cuInit
 cuDeviceGetCount = cuda.cuDeviceGetCount
 cuCtxGetCurrent = cuda.cuCtxGetCurrent
 cuDeviceGet = cuda.cuDeviceGet
+cuDeviceGetName = cuda.cuDeviceGetName
+cuDeviceGetAttribute = cuda.cuDeviceGetAttribute
+cuMemGetInfo = cuda.cuMemGetInfo
 cuCtxCreate = cuda.cuCtxCreate
 cuModuleLoad = cuda.cuModuleLoad
 cuCtxSynchronize = cuda.cuCtxSynchronize
@@ -14,3 +17,11 @@ cuMemcpyDtoH = cuda.cuMemcpyDtoH
 cuMemFree = cuda.cuMemFree
 cuLaunchKernel = cuda.cuLaunchKernel
 cuCtxDestroy = cuda.cuCtxDestroy
+
+# Macros
+COMPUTE_CAPABILITY_MAJOR = cuda.CUdevice_attribute.CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR
+COMPUTE_CAPABILITY_MINOR = cuda.CUdevice_attribute.CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR
+MULTIPROCESSOR_COUNT = cuda.CUdevice_attribute.CU_DEVICE_ATTRIBUTE_MULTIPROCESSOR_COUNT
+MAX_THREAD_PER_MULTIPROCESSOR = cuda.CUdevice_attribute.CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_MULTIPROCESSOR
+CLOCK_RATE = cuda.CUdevice_attribute.CU_DEVICE_ATTRIBUTE_CLOCK_RATE
+MEMORY_CLOCK_RATE = cuda.CUdevice_attribute.CU_DEVICE_ATTRIBUTE_MEMORY_CLOCK_RATE
