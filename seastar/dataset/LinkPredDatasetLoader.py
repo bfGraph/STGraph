948dd44663e61dfb4c6223beab01305af59e175e
JoelMathewC <joelmathewcherian@gmail.com>
2023-06-21 22:48:34 +0400

    :zap: improved LinkPredDataLoader _get_edge_info() calculation

--- seastar/dataset/LinkPredDatasetLoader.py
+++ seastar/dataset/LinkPredDatasetLoader.py
@@ -28,7 +28,6 @@ class LinkPredDatasetLoader:
 
         self._load_dataset()
         self.total_timestamps = self._dataset["time_periods"]
-        self._dataset["time_periods"] = 4
         self.for_seastar = for_seastar
         
         self._get_edge_info()
@@ -58,21 +57,16 @@ class LinkPredDatasetLoader:
         edge_list = []
         updates = self._dataset["edge_mapping"]["edge_index"]
 
-        updated_graph = updates["0"]["add"]
-        edge_list.append(updated_graph)
-
+        working_set = set([(edge[0], edge[1]) for edge in updates["0"]["add"]])
+        edge_list.append(list(working_set))
         for time in range(1, self.total_timestamps):
-            add_edges = updates[str(time)]["add"]
-            delete_edges = updates[str(time)]["delete"]
-
-            updated_graph = copy.deepcopy(updated_graph)
-            updated_graph = updated_graph[len(delete_edges):] + add_edges
-            edge_list.append(updated_graph)
+            working_set = working_set.union(set([(edge[0], edge[1]) for edge in updates[str(time)]["add"]])).difference(set([(edge[0], edge[1]) for edge in updates[str(time)]["delete"]]))
+            edge_list.append(list(working_set))
         
         if self.for_seastar:
-            self._edge_list = [[(edge[0], edge[1]) for edge in edge_lst_t] for edge_lst_t in edge_list]
+            self._edge_list = edge_list
         else:
-            self._edge_list = [np.array(edge_lst_t).T for edge_lst_t in edge_list]
+            self._edge_list = [np.array([[edge[0], edge[1]] for edge in edge_lst_t]).T for edge_lst_t in edge_list]
     
     def _preprocess_pos_neg_edges(self):
         updates = self._dataset["edge_mapping"]["edge_index"]
@@ -86,12 +80,6 @@ class LinkPredDatasetLoader:
             pos_neg_edge_list.append(pos_edges_tup + neg_edges_tup)
             pos_neg_edge_label_list.append([(edge[0], edge[1], 1) for edge in pos_edges_tup] + [(edge[0], edge[1], 0) for edge in neg_edges_tup])
 
-        # if self.for_seastar:
-        #     self._pos_neg_edge_list = [np.array(edge_list) for edge_list in pos_neg_edge_list]
-        #     # pos_neg_edge_label_list.sort(key=lambda x: (x[1], x[0]))
-        #     # self._pos_neg_edge_label_list = [np.array([edge[2] for edge in edge_list]) for edge_list in pos_neg_edge_label_list]
-        #     self._pos_neg_edge_label_list = [np.array([edge[2] for edge in edge_list]) for edge_list in pos_neg_edge_label_list]
-        # else:
         self._pos_neg_edge_list = [np.array(edge_list).T for edge_list in pos_neg_edge_list]
         self._pos_neg_edge_label_list = [np.array([edge[2] for edge in edge_list]) for edge_list in pos_neg_edge_label_list]
 
