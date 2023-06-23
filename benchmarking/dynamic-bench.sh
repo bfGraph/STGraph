b7ebf5afac94bb3f237f274a8bd25efc9245a0f0
JoelMathewC <joelmathewcherian@gmail.com>
2023-06-23 21:08:39 +0400

    🔨Changed num-epochs to 10 in bench scripts

--- benchmarking/dynamic-bench.sh
+++ benchmarking/dynamic-bench.sh
@@ -12,7 +12,7 @@ do
         for feat_size in {8..80..8}
         do
             hidden_units=$((feat_size*2))
-            python3 train.py --dataset $dataset --num-epochs 5 --slide-size $slide_size --num-hidden $hidden_units --feat-size $feat_size > ../../results/dynamic-temporal/pygt_$dataset\_Twhole_S$i\_Bwhole_H$hidden_units\_F$feat_size.txt
+            python3 train.py --dataset $dataset --num-epochs 10 --slide-size $slide_size --num-hidden $hidden_units --feat-size $feat_size > ../../results/dynamic-temporal/pygt_$dataset\_Twhole_S$i\_Bwhole_H$hidden_units\_F$feat_size.txt
             echo "Finished executing PyG-T sx-mathoverflow script for F=$feat_size"
         done
     done
@@ -26,7 +26,7 @@ do
         for feat_size in {8..80..8}
         do
             hidden_units=$((feat_size*2))
-            python3 train.py --dataset $dataset --num-epochs 5 --slide-size $slide_size --num-hidden $hidden_units --feat-size $feat_size --backprop-every 30 > ../../results/dynamic-temporal/pygt_$dataset\_Twhole_S$i\_B30_H$hidden_units\_F$feat_size.txt
+            python3 train.py --dataset $dataset --num-epochs 10 --slide-size $slide_size --num-hidden $hidden_units --feat-size $feat_size --backprop-every 30 > ../../results/dynamic-temporal/pygt_$dataset\_Twhole_S$i\_B30_H$hidden_units\_F$feat_size.txt
             echo "Finished executing PyG-T wiki-talk-temporal script for F=$feat_size"
         done
     done
@@ -47,7 +47,7 @@ do
             for feat_size in {8..80..8}
             do
                 hidden_units=$((feat_size*2))
-                python3 train.py --type $type --dataset $dataset --num-epochs 5 --slide-size $slide_size --num-hidden $hidden_units --feat-size $feat_size > ../../results/dynamic-temporal/seastar_$type\_$dataset\_Twhole_S$i\_Bwhole_H$hidden_units\_F$feat_size.txt
+                python3 train.py --type $type --dataset $dataset --num-epochs 10 --slide-size $slide_size --num-hidden $hidden_units --feat-size $feat_size > ../../results/dynamic-temporal/seastar_$type\_$dataset\_Twhole_S$i\_Bwhole_H$hidden_units\_F$feat_size.txt
                 echo "Finished executing Seastar sx-mathoverflow script for F=$feat_size"
             done
         done
@@ -64,7 +64,7 @@ do
             for feat_size in {8..80..8}
             do
                 hidden_units=$((feat_size*2))
-                python3 train.py --type $type --dataset $dataset --num-epochs 5 --slide-size $slide_size --num-hidden $hidden_units --feat-size $feat_size --backprop-every 30 > ../../results/dynamic-temporal/seastar_$type\_$dataset\_Twhole_S$i\_B30_H$hidden_units\_F$feat_size.txt
+                python3 train.py --type $type --dataset $dataset --num-epochs 10 --slide-size $slide_size --num-hidden $hidden_units --feat-size $feat_size --backprop-every 30 > ../../results/dynamic-temporal/seastar_$type\_$dataset\_Twhole_S$i\_B30_H$hidden_units\_F$feat_size.txt
                 echo "Finished executing Seastar wiki-talk-temporal script for F=$feat_size"
             done
         done
