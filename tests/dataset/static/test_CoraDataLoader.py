a45caa446284d65e7ee0e961fa2ff5ff3b0918c7
nithinmanoj10 <nithinp.manoj@gmail.com>
2023-10-22 17:57:19 +0530

    ➕ Added more test checks for CoraDataLoader

--- tests/dataset/static/test_CoraDataLoader.py
+++ tests/dataset/static/test_CoraDataLoader.py
@@ -1,3 +1,6 @@
+import numpy as np
+import urllib.request
+
 from stgraph.dataset import CoraDataLoader
 
 
@@ -7,7 +10,18 @@ class TestCoraDataLoader:
     def test_init(self):
         assert self.cora.name == "Cora", "Incorrect name for the CoraDataLoader"
         assert self.cora._verbose == False, "Verbose flag for Cora not set to False"
-        assert self.cora._train_mask == None, "Train mask for Cora is None"
-        assert self.cora._test_mask == None, "Test mask for Cora is None"
-        assert self.cora._train_split != 0.75, "Train split not set to 0.75"
-        assert self.cora._test_split != 0.25, "Test split not set to 0.25"
+
+        assert isinstance(
+            self.cora._train_mask, np.ndarray
+        ), "Train mask for Cora is not a numpy array"
+
+        assert isinstance(
+            self.cora._test_mask, np.ndarray
+        ), "Test mask for Cora is not a numpy array"
+
+        assert self.cora._train_split == 0.75, "Train split not set to 0.75"
+        assert self.cora._test_split == 0.25, "Test split not set to 0.25"
+
+        assert (
+            urllib.request.urlopen(self.cora._url).getcode() == 200
+        ), "Cora dataset URL is not available"
