a60224a3cd8e65d9dcfcb43aab3b16124963d8b6
nithinmanoj10 <nithinp.manoj@gmail.com>
2023-11-06 21:56:06 +0530

    ➕ Created STGraphDynamicDataset base class

---
+++ stgraph/dataset/dynamic/STGraphDynamicDataset.py
@@ -0,0 +1,27 @@
+"""Base class for all STGraph dynamic graph datasets"""
+
+from stgraph.dataset.STGraphDataset import STGraphDataset
+
+
+class STGraphDynamicDataset(STGraphDataset):
+    r"""Base class for dynamic graph datasets
+
+    This class is a subclass of ``STGraphDataset`` and provides the base structure for
+    handling dynamic graph datasets."""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._init_graph_data()
+
+    def _init_graph_data(self) -> dict:
+        r"""Initialize graph meta data for a dynamic dataset.
+
+        The ``num_nodes``, ``num_edges``, ``total_timestamps`` keys are set to value 0
+        """
+        self.gdata["num_nodes"] = {}
+        self.gdata["num_edges"] = {}
+        self.gdata["total_timestamps"] = 0
+
+        self._lags = 0
+        self._cutoff_time = None
