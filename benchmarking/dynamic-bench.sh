62c6e97892081dad45449d11b6f7f61ba57df9ae
JoelMathewC <joelmathewcherian@gmail.com>
2024-01-26 22:35:34 +0530

    updating dynamic bench script to exclude naive for large datasets

--- benchmarking/dynamic-bench.sh
+++ benchmarking/dynamic-bench.sh
@@ -63,7 +63,7 @@ cd dynamic-temporal-tgcn/stgraph
 
 echo "Running STGraph on all script for different slide sizes"
 
-for dataset in math wikitalk askubuntu superuser stackoverflow reddit_title reddit_body email bitcoin_otc
+for dataset in math askubuntu superuser reddit_title reddit_body email bitcoin_otc
 do
     for type in naive gpma
     do
@@ -75,10 +75,39 @@ do
     done
 done
 
-echo "Running STGraph (wikitalk, askubuntu, superuser, stackoverflow) script for different feature sizes"
-for dataset in wikitalk askubuntu superuser stackoverflow
+for dataset in wikitalk stackoverflow
+do
+    for type in gpma
+    do
+        for slide_size in 2.0 4.0 6.0 8.0 10.0
+        do
+            python3 train.py --type $type --dataset $dataset --num-epochs 100 --slide-size $slide_size --num-hidden 64 --feat-size 32 --backprop-every 5 > ../../results/dynamic-temporal/stgraph_$type\_$dataset\_Twhole_S$slide_size\_B5_H64_F32.txt
+            echo "Finished executing STGraph $dataset for type $type script for S=$slide_size"
+        done
+    done
+done
+
+echo "Running STGraph (askubuntu, superuser) script for different feature sizes"
+for dataset in askubuntu superuser
 do
     for type in naive gpma
+    do
+        for slide_size in 5.0
+        do
+            for feat_size in {8..80..8}
+            do
+                hidden_units=$((feat_size*2))
+                python3 train.py --type $type --dataset $dataset --num-epochs 100 --slide-size $slide_size --num-hidden $hidden_units --feat-size $feat_size --backprop-every 5 > ../../results/dynamic-temporal/stgraph_$type\_$dataset\_Twhole_S$slide_size\_B5_H$hidden_units\_F$feat_size.txt
+                echo "Finished executing STGraph $dataset for type $type for F=$feat_size"
+            done
+        done
+    done
+done
+
+echo "Running STGraph (wikitalk, stackoverflow) script for different feature sizes"
+for dataset in wikitalk stackoverflow
+do
+    for type in gpma
     do
         for slide_size in 5.0
         do
