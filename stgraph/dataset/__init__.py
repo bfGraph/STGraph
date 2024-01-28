3b26e293e4be15b0b816bccc549b30938343b644
nithinmanoj10 <nithinp.manoj@gmail.com>
2024-01-28 15:42:02 +0530

    ➕ Create __init__.py in stgraph.dataset module

---
+++ stgraph/dataset/__init__.py
@@ -0,0 +1,10 @@
+"""Dataset loader provided by STGraph."""
+
+from stgraph.dataset.dynamic.england_covid_dataloader import EnglandCovidDataLoader
+from stgraph.dataset.static.cora_dataloader import CoraDataLoader
+from stgraph.dataset.temporal.hungarycp_dataloader import HungaryCPDataLoader
+from stgraph.dataset.temporal.metrla_dataloader import METRLADataLoader
+from stgraph.dataset.temporal.montevideobus_dataloader import MontevideoBusDataLoader
+from stgraph.dataset.temporal.pedalme_dataloader import PedalMeDataLoader
+from stgraph.dataset.temporal.wikimath_dataloader import WikiMathDataLoader
+from stgraph.dataset.temporal.windmilloutput_dataloader import WindmillOutputDataLoader
