d98ec4b64a9484a2a9a5c0a66be54174861b2048
nithinmanoj10 <nithinp.manoj@gmail.com>
2024-01-31 23:04:12 +0530

    📝 Moved CSR Readme
    And modified the script to build the CSR .so file

--- stgraph/graph/build_static.sh
+++ stgraph/graph/build_static.sh
@@ -1,6 +1,6 @@
 echo "🔨 Building csr"
-cd static/
+cd static/csr
 /usr/local/cuda-11.7/bin/nvcc $(python3 -m pybind11 --includes) -shared -rdc=true --compiler-options '-fPIC' -D__CDPRT_SUPPRESS_SYNC_DEPRECATION_WARNING -o csr.so csr.cu
 echo "✅ csr build completed"
-cd ..
+cd ../..
 echo ""
