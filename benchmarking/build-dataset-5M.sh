bb25953979fd416cf2b4437c3c48c5d555507735
JoelMathewC <joelmathewcherian@gmail.com>
2024-02-03 10:08:23 +0530

    correct dataset load scripts

--- benchmarking/build-dataset-5M.sh
+++ benchmarking/build-dataset-5M.sh
@@ -4,8 +4,8 @@ cd dataset/dynamic-temporal
 
 for i in 2.0 4.0 5.0 6.0 8.0 10.0
 do
-    python3 ../preprocessing/preprocess_temporal_data.py --dataset wiki-talk-temporal --base 3500000 --cutoff-time 5000000 --percent-change $i
-    python3 ../preprocessing/preprocess_temporal_data.py --dataset sx-stackoverflow --base 10000000 --cutoff-time 5000000 --percent-change $i
+    python3 ../preprocessing/preprocess_temporal_data.py --dataset wiki-talk-temporal --base 2500000 --cutoff-time 5000000 --percent-change $i
+    python3 ../preprocessing/preprocess_temporal_data.py --dataset sx-stackoverflow --base 2500000 --cutoff-time 5000000 --percent-change $i
 done
 
 cd ../../
